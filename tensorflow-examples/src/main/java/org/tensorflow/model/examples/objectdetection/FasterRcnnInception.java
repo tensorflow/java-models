3002c297f352d450b94f5723b56922d43ab095a2
pyrator <the.keith.hall@googlemail.com>
2021-04-06 07:47:39 +0100

    Added copyright and markdown file closed tensors plus other tidy up,
    Amended file to match requested changes

--- tensorflow-examples/src/main/java/org/tensorflow/model/examples/objectdetection/FasterRcnnInception.java
+++ tensorflow-examples/src/main/java/org/tensorflow/model/examples/objectdetection/FasterRcnnInception.java
@@ -1,11 +1,25 @@
+/*
+ *  Copyright 2020 The TensorFlow Authors. All Rights Reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *  =======================================================================
+ */
+
 package org.tensorflow.model.examples.objectdetection;
 /*
 
-download the model from https://tfhub.dev/tensorflow/faster_rcnn/inception_resnet_v2_1024x1024/1
-
-unzip then untar the model to a local folder - I've used models/faster_rcnn_inception_resnet_v2_1024x1024
 From the web page this is the output dictionary
-Add some test images into a testimages folder
+
 num_detections: a tf.int tensor with only one value, the number of detections [N].
 detection_boxes: a tf.float32 tensor of shape [N, 4] containing bounding box coordinates in the following order: [ymin, xmin, ymax, xmax].
 detection_classes: a tf.int tensor of shape [N] containing detection class index from the label file.
@@ -78,13 +92,23 @@ Defined Functions:
         Argument #1
           input_tensor: TensorSpec(shape=(1, None, None, 3), dtype=tf.uint8, name='input_tensor')
 
-So it appears there's a discrepancy between the web page and running saved_model_cli.
+So it appears there's a discrepancy between the web page and running saved_model_cli as
+num_detections: a tf.int tensor with only one value, the number of detections [N].
+but the actual tensor is DT_FLOAT according to saved_model_cli
+also the web page states
+detection_classes: a tf.int tensor of shape [N] containing detection class index from the label file.
+but again the actual tensor is DT_FLOAT according to saved_model_cli.
 */
 
-import org.tensorflow.*;
+
+import org.tensorflow.Graph;
+import org.tensorflow.SavedModelBundle;
+import org.tensorflow.Session;
+import org.tensorflow.Tensor;
 import org.tensorflow.ndarray.Shape;
 import org.tensorflow.op.Ops;
 import org.tensorflow.op.core.Constant;
+import org.tensorflow.op.core.Reshape;
 import org.tensorflow.op.image.DecodeJpeg;
 import org.tensorflow.op.io.ReadFile;
 import org.tensorflow.types.TFloat32;
@@ -96,9 +120,16 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.text.DecimalFormat;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.TreeMap;
+
+
+/**
+ * Loads an image using ReadFile and DecodeJpeg and then uses the saved model
+ * faster_rcnn/inception_resnet_v2_1024x1024/1 to detect objects with a detection score greater than 0.3
+ */
 
 public class FasterRcnnInception {
 
@@ -199,10 +230,12 @@ public class FasterRcnnInception {
     public static void main(String[] params) {
 
 
-        // get path to model folder (currently in resources
+        // get path to model folder
         String modelPath = "models/faster_rcnn_inception_resnet_v2_1024x1024";
         // load saved model
         SavedModelBundle model = SavedModelBundle.load(modelPath, "serve");
+
+        //create a map of the COCO 2017 labels
         TreeMap<Float, String> cocoTreeMap = new TreeMap<>();
         float cocoCount = 0;
         for (String cocoLabel : cocoLabels) {
@@ -225,78 +258,71 @@ public class FasterRcnnInception {
 
             DecodeJpeg.Options options = DecodeJpeg.channels(3L);
             DecodeJpeg decodeImage = tf.image.decodeJpeg(readFile.contents(), options);
+
             //fetch image from file
             TUint8 outputImage = (TUint8) runner.fetch(decodeImage).run().get(0);
             Shape imageShape = outputImage.shape();
+            //dimensions of test image
             long[] shapeArray = imageShape.asArray();
-
+            //reshape the tensor to 4D for input to model
+            Reshape<TUint8> reshape = tf.reshape(tf.constant(outputImage),
+                    tf.array(1,
+                            outputImage.shape().asArray()[0],
+                            outputImage.shape().asArray()[1],
+                            outputImage.shape().asArray()[2]
+                    )
+            );
+            TUint8 reshapeTensor = (TUint8) runner.fetch(reshape).run().get(1);
+            Map<String, Tensor> feedDict = new HashMap<>();
             //The given SavedModel SignatureDef input
-            Map<String, Tensor> feed_dict = new HashMap<>();
-            feed_dict.put("input_tensor", reshapeTensor(outputImage));
+            feedDict.put("input_tensor", reshapeTensor);//reshapeTensor(outputImage));
             //The given SavedModel MetaGraphDef key
-            Map<String, Tensor> outputTensorMap = model.function("serving_default").call(feed_dict);
+            Map<String, Tensor> outputTensorMap = model.function("serving_default").call(feedDict);
 
             //detection_classes is a model output name
-            TFloat32 detectionClasses = (TFloat32) outputTensorMap.get("detection_classes");
-            TFloat32 detectionBoxes = (TFloat32) outputTensorMap.get("detection_boxes");
-            TFloat32 numDetections = (TFloat32) outputTensorMap.get("num_detections");
-            TFloat32 detectionScores = (TFloat32) outputTensorMap.get("detection_scores");
+            try (TFloat32 detectionClasses = (TFloat32) outputTensorMap.get("detection_classes");
+                 TFloat32 detectionBoxes = (TFloat32) outputTensorMap.get("detection_boxes");
+                 TFloat32 numDetections = (TFloat32) outputTensorMap.get("num_detections");
+                 TFloat32 detectionScores = (TFloat32) outputTensorMap.get("detection_scores")) {
 
-            int numDetects = (int) numDetections.getFloat(0);
-            if (numDetects > 0) {
-                try {
-                    BufferedImage bufferedImage = ImageIO.read(new File(imagePath));
-                    Graphics2D graphics2D = bufferedImage.createGraphics();
+                int numDetects = (int) numDetections.getFloat(0);
+                if (numDetects > 0) {
+                    try {
+                        BufferedImage bufferedImage = ImageIO.read(new File(imagePath));
+                        Graphics2D graphics2D = bufferedImage.createGraphics();
 
-                    //TODO tf.image.combinedNonMaxSuppression
-                    for (int n = 0; n < numDetects; n++) {
-                        //put probability and position in outputMap
-                        if(detectionScores.getFloat(0,n)> 0.3f) {
-                            Float classVal = detectionClasses.getFloat(0, n);
-                            //TODO tf.image.drawBoundingBoxes
-                            int x1 = (int) (shapeArray[1] * detectionBoxes.getFloat(0, n, 1));
-                            int y1 = (int) (shapeArray[0] * detectionBoxes.getFloat(0, n, 0));
-                            int x2 = (int) (shapeArray[1] * detectionBoxes.getFloat(0, n, 3));
-                            int y2 = (int) (shapeArray[0] * detectionBoxes.getFloat(0, n, 2));
-                            graphics2D.setPaint(Color.RED);
-                            graphics2D.setStroke(new BasicStroke(5));
-                            graphics2D.drawRect(x1, y1, x2 - x1, y2 - y1);
-                            graphics2D.setPaint(Color.BLACK);
-                            graphics2D.drawString(cocoTreeMap.get(classVal - 1) + " " + classVal, x1, y1);
+                        //TODO tf.image.combinedNonMaxSuppression
+                        for (int n = 0; n < numDetects; n++) {
+                            //put probability and position in outputMap
+                            float detectionScore = detectionScores.getFloat(0, n);
+                            //only include those classes with detection score greater than 0.3f
+                            if (detectionScore > 0.3f) {
+                                Float classVal = detectionClasses.getFloat(0, n);
+                                //TODO tf.image.drawBoundingBoxes
+                                int x1 = (int) (shapeArray[1] * detectionBoxes.getFloat(0, n, 1));
+                                int y1 = (int) (shapeArray[0] * detectionBoxes.getFloat(0, n, 0));
+                                int x2 = (int) (shapeArray[1] * detectionBoxes.getFloat(0, n, 3));
+                                int y2 = (int) (shapeArray[0] * detectionBoxes.getFloat(0, n, 2));
+                                graphics2D.setPaint(Color.RED);
+                                graphics2D.setStroke(new BasicStroke(5));
+                                graphics2D.drawRect(x1, y1, x2 - x1, y2 - y1);
+                                graphics2D.setPaint(Color.BLACK);
+                                //add a label with percentage score 
+                                graphics2D.drawString(cocoTreeMap.get(classVal - 1) + " " +
+                                        (new DecimalFormat("#.##").format(detectionScore * 100)) +
+                                        "%", x1, y1);
+                            }
                         }
+                        //TODO tf.image.encodeJpeg
+                        ImageIO.write(bufferedImage, "jpg", new File("image2rcnn.jpg"));
+                    } catch (IOException e) {
+                        System.err.println("Exception with writing image " + e.getMessage());
                     }
-                    //TODO tf.image.encodeJpeg
-                    ImageIO.write(bufferedImage, "jpg", new File("image2rcnn.jpg"));
-                } catch (IOException e) {
-
                 }
+
             }
+            reshapeTensor.close();
+            outputImage.close();
         }
     }
-
-    /**
-     * return a 4D tensor from 3D tensor
-     *
-     * @param tUint8Tensor 3D tensor
-     * @return 4D tensor
-     */
-    private static TUint8 reshapeTensor(TUint8 tUint8Tensor) {
-        Ops tf = Ops.create();
-        return tf.reshape(tf.constant(tUint8Tensor),
-                tf.array(1,
-                        tUint8Tensor.shape().asArray()[0],
-                        tUint8Tensor.shape().asArray()[1],
-                        tUint8Tensor.shape().asArray()[2]
-                )
-        ).asTensor();
-    }
-
-    private static TUint8 drawBoundingBoxes(TUint8 images, TFloat32 boxes, TFloat32 colors ){
-        Ops tf = Ops.create();
-        Operand<TUint8> imagesOp = tf.constant(images);
-        Operand<TFloat32> boxesOp = tf.constant(boxes);
-        Operand<TFloat32> colorsOp = tf.constant(colors);
-        return tf.image.drawBoundingBoxes(imagesOp,boxesOp,colorsOp).asTensor();
-    }
-
 }
---
+++ tensorflow-examples/src/main/java/org/tensorflow/model/examples/objectdetection/Readme.md
@@ -0,0 +1,13 @@
+# FasterRcnnInception
+
+Download the model from https://tfhub.dev/tensorflow/faster_rcnn/inception_resnet_v2_1024x1024/1
+
+Unzip then untar the model to a local folder - I've used models/faster_rcnn_inception_resnet_v2_1024x1024.
+
+Create a testimages folder then add some test images into a testimages folder
+
+
+### Example
+Using the image2.jpg image from https://github.com/tensorflow/models/tree/master/research/object_detection/test_images
+![image2rcnn.jpg.](image2rcnn.jpg "Beach")
+
